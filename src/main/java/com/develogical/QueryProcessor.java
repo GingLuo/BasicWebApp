3f3b46151e9aeab918795acc9f0cc9b69b2e4510
GingLuo <yichuanl@andrew.cmu.edu>
2022-09-22 11:24:23 -0400

    add largest number query and test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,5 +1,7 @@
 package com.develogical;
 
+import java.util.Arrays;
+
 public class QueryProcessor {
 
     public String process(String query) {
@@ -9,6 +11,24 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }else if (query.toLowerCase().contains("name")){
             return "ssggggghih";
+        }else if (query.toLowerCase().contains("number")){
+            // Pattern regex = Pattern.compile("(\\d+)(, \\d+)*");
+            // Matcher matcher = regex.matcher(query);
+            // int res = Math.max.apply(null, query.match(/\d+/g));
+            // System.out.println(matcher.find());
+
+            // System.out.println(matcher.group(0));
+            // System.out.println(matcher.group(1));
+            // System.out.println(matcher.group(2));
+            // System.out.println(matcher.group(3));
+            String[] parts = query.split(",");
+            String[] newParts = Arrays.copyOfRange(parts, 1, parts.length);
+            String[] colons = parts[0].split(":");
+            int res = Integer.parseInt(colons[1].trim());
+            for (String s : newParts){
+                res = Integer.max(res, Integer.parseInt(s.trim()));
+            }
+            return Integer.toString(res);
         }
         return "";
     }
